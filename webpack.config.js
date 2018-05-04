7d3908b15cada914997ae3d664ee29759cb8899d
diego pancera <diegopancera@MacBook-Pro-de-open.local>
2018-05-04 16:11:59 -0300

    Webpack config initial

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,80 @@
-const webpack = require('webpack');
+'use strict'
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+function resolve (dir) {
+    return path.join(__dirname, '..', dir)
+}
 
 module.exports = {
+    devtool: 'cheap-module-eval-source-map',
     entry: [
         'react-hot-loader/patch',
         './src/index.js'
     ],
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.[hash:7].js'
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+        alias: {
+            '@': resolve('src'),
+        }
+    },
+    devServer: {
+        clientLogLevel: 'warning',
+        contentBase: './static',
+        publicPath: "/",
+        hot: true,
+        host: 'localhost',
+        port: 8080,
+        open: false,
+        overlay: { warnings: false, errors: true }
+    },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                  {
+                    loader: "html-loader"
+                  }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                  limit: 10000,
+                  name: path.posix.join('images/[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                  limit: 10000,
+                  name: path.posix.join('fonts/[name].[hash:7].[ext]')
+                }
             }
         ]
     },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new HtmlWebPackPlugin({
+            template: "./static/index.html",
+            filename: "./index.html"
+        }),
     ],
-    output: {
-        path: __dirname + '/public/dist',
-        publicPath: '/public/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './public',
-        hot: true
-    }
 };
